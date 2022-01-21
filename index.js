c9b16759bf4c2f66df52ba92fd8eb2081b2e5480
Pradeep Sahu <specialreaderman.com@gmail.com>
2022-01-22 03:37:40 +0530

    Node server

---
+++ index.js
@@ -0,0 +1,11 @@
+//jshint esversion: 6
+
+
+const supervillains = require('supervillains');
+
+supervillains.all;
+//=> ['Abattoir', 'Able Crown', …]
+
+var superVillains = supervillains.random();
+//=> 'Mud Pack'
+console.log(superVillains);
